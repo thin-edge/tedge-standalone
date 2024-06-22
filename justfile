3dbf60439d4848adbbf0d9499c70a729c28d2de4
Reuben Miller <reuben.d.miller@gmail.com>
2024-06-23 00:01:14 +0200

    include tedge binary in package

--- justfile
+++ justfile
@@ -5,4 +5,5 @@ PACKAGE := "tedge-standalone"
 build target *args:
     ./scripts/build.sh {{target}} {{args}}
     cp "binaries/zig-mosquitto/zig-out/bin/mosquitto-{{target}}" src/tedge/bin/mosquitto
+    cp "tedge-{{target}}" src/tedge/bin/tedge
     tar czvf "{{PACKAGE}}-{{target}}.tar.gz" --owner=0 --group=0 --no-same-owner --no-same-permissions -C src ./tedge
--- scripts/build.sh
+++ scripts/build.sh
@@ -18,3 +18,31 @@ ln -sf ../mosquitto mosquitto
 
 $ZIG build -Doptimize=ReleaseSmall -Dtarget="$TARGET"
 mv zig-out/bin/mosquitto "zig-out/bin/mosquitto-$TARGET"
+
+if command -V upx; then
+    upx --lzma --best "zig-out/bin/mosquitto-$TARGET"
+fi
+
+# Download tedge
+cd ../../
+
+TEDGE_VERSION="1.1.2-rc139+g4e94ab6"
+case "$TARGET" in
+    aarch64-linux-musl)
+        TEDGE_URL="https://dl.cloudsmith.io/public/thinedge/tedge-main/raw/names/tedge-arm64/versions/$TEDGE_VERSION/tedge.tar.gz"
+        ;;
+    arm-linux-musleabihf)
+        TEDGE_URL="https://dl.cloudsmith.io/public/thinedge/tedge-main/raw/names/tedge-armv7/versions/$TEDGE_VERSION/tedge.tar.gz"
+        ;;
+    arm-linux-musleabi)
+        TEDGE_URL="https://dl.cloudsmith.io/public/thinedge/tedge-main/raw/names/tedge-armv5/versions/$TEDGE_VERSION/tedge.tar.gz"
+        ;;
+esac
+
+echo "Download the tedge binary: $TEDGE_URL" >&2
+wget -O "tedge-$TARGET.tar.gz" "$TEDGE_URL"
+tar xzvf "tedge-$TARGET.tar.gz"
+mv tedge "tedge-$TARGET"
+if command -V upx; then
+    upx --lzma --best "tedge-$TARGET"
+fi
