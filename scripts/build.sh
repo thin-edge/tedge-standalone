f1a0d48072f1ab56e846a8af0cc601b16758550a
Reuben Miller <reuben.d.miller@gmail.com>
2024-06-23 00:22:01 +0200

    extend architectures

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,6 +2,11 @@
 set -ex
 TARGET="$1"
 
+SKIP_UPX=0
+if [ "$TARGET" = "riscv64-linux" ]; then
+    SKIP_UPX=1
+fi
+
 ZIG=zig
 if ! command -V "$ZIG" >/dev/null 2>&1; then
     python3 -m venv .venv ||:
@@ -19,8 +24,10 @@ ln -sf ../mosquitto mosquitto
 $ZIG build -Doptimize=ReleaseSmall -Dtarget="$TARGET"
 mv zig-out/bin/mosquitto "zig-out/bin/mosquitto-$TARGET"
 
-if command -V upx; then
-    upx --lzma --best "zig-out/bin/mosquitto-$TARGET"
+if [ "$SKIP_UPX" -ne 1 ]; then
+    if command -V upx; then
+        upx --lzma --best "zig-out/bin/mosquitto-$TARGET"
+    fi
 fi
 
 # Download tedge
@@ -37,12 +44,28 @@ case "$TARGET" in
     arm-linux-musleabi)
         TEDGE_URL="https://dl.cloudsmith.io/public/thinedge/tedge-main/raw/names/tedge-armv5/versions/$TEDGE_VERSION/tedge.tar.gz"
         ;;
+    x86_64-linux)
+        TEDGE_URL="https://dl.cloudsmith.io/public/thinedge/tedge-main/raw/names/tedge-amd64/versions/$TEDGE_VERSION/tedge.tar.gz"
+        ;;
+    x86-linux)
+        TEDGE_URL="https://dl.cloudsmith.io/public/thinedge/tedge-main/raw/names/tedge-i386/versions/$TEDGE_VERSION/tedge.tar.gz"
+        ;;
+    riscv64-linux)
+        TEDGE_URL="https://dl.cloudsmith.io/public/thinedge/tedge-main/raw/names/tedge-riscv64/versions/$TEDGE_VERSION/tedge.tar.gz"
+        ;;
+    *)
+        echo "Unsupported target. No thin-edge.io binary is available for this target. $TARGET" >&2
+        exit 1
+        ;;
 esac
 
 echo "Download the tedge binary: $TEDGE_URL" >&2
 wget -O "tedge-$TARGET.tar.gz" "$TEDGE_URL"
 tar xzvf "tedge-$TARGET.tar.gz"
 mv tedge "tedge-$TARGET"
-if command -V upx; then
-    upx --lzma --best "tedge-$TARGET"
+
+if [ "$SKIP_UPX" -ne 1 ]; then
+    if command -V upx ; then
+        upx --lzma --best "tedge-$TARGET"
+    fi
 fi
