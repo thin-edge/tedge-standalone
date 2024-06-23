c69d05d409a4a358d8de133945bb326d8daed8e0
Reuben Miller <reuben.d.miller@gmail.com>
2024-06-23 11:05:30 +0200

    add install script and instructions

--- README.md
+++ README.md
@@ -4,6 +4,20 @@
 
 * create wrapper script for tedge init manager interactions, though maybe it is not needed
 
+## Install
+
+**Using wget**
+
+```sh
+wget -q -O - https://raw.githubusercontent.com/thin-edge/tedge-standalone/main/install.sh | sh -s
+```
+
+**Using curl**
+
+```sh
+curl -fsSL https://raw.githubusercontent.com/thin-edge/tedge-standalone/main/install.sh | sh -s
+```
+
 ## Bootstrapping
 
 Run the start.sh script to run the bootstrap process to create a certificate and connect thin-edge.io
--- install.sh
+++ install.sh
@@ -1,43 +1,96 @@
 #!/bin/sh
 set -e
 
-INSTALL_PATH=/data
-VERSION=0.0.1-rc.3
+INSTALL_PATH="${INSTALL_PATH:-/data}"
+VERSION="${VERSION:-0.0.1-rc.3}"
 
-ARCH=$(uname -m)
-TARGET_ARCH=
-case "$ARCH" in
-    arm64|aarch64)
-        TARGET_ARCH=arm64
-        ;;
-    armv7*)
-        TARGET_ARCH=armhf
-        ;;
-    armel)
-        TARGET_ARCH=armel
-        ;;
-    x86_64|amd64)
-        TARGET_ARCH=amd64
-        ;;
-    i386|x86)
-        TARGET_ARCH=i386
-        ;;
-    *)
-        echo "Unsupported architecture. arch=$ARCH" >&2
-        exit 1
-        ;;
-esac
+usage() {
+    cat << EOT
+Install thin-edge.io standalone package for running on resource constrained devices
+without any accessible init system.
 
-cd /tmp
-wget -q "https://github.com/thin-edge/tedge-standalone/releases/download/$VERSION/tedge-standalone-${TARGET_ARCH}.tar.gz"
+USAGE
+    $0 [--install-path <path>] [--version <version>]
 
-mkdir -p "$INSTALL_PATH"
-echo "Installing thin-edge.io to $INSTALL_PATH"
-tar xzf /tmp/tedge-standalone-*.tar.gz -C "$INSTALL_PATH"
-rm -f /tmp/tedge-standalone-*.tar.gz
+ARGUMENTS
+  --install-path <path>         Install path. Defaults to $INSTALL_PATH
+  --version <version>           Version to install. Defaults to $VERSION
 
-echo
-echo "Configure and start thin-edge.io using the following command:"
-echo
-echo "    $INSTALL_PATH/start.sh"
-echo
+EXAMPLE
+
+    $0
+    # Install with default settings
+
+    $0 --install-path /custom
+    # Install under a custom location
+
+EOT
+}
+
+while [ $# -gt 0 ]; do
+    case "$1" in
+        --install-path)
+            INSTALL_PATH="$2"
+            shift
+            ;;
+        --version)
+            VERSION="$2"
+            shift
+            ;;
+        --help|-h)
+            usage
+            exit 0
+            ;;
+        --*|-*)
+            echo "Unknown flags. $1" >&2
+            exit 1
+            ;;
+        *)
+            echo "Unexpected positional arguments" >&2
+            exit 1
+            ;;
+    esac
+    shift
+done
+
+main() {
+    ARCH=$(uname -m)
+    TARGET_ARCH=
+    case "$ARCH" in
+        arm64|aarch64)
+            TARGET_ARCH=arm64
+            ;;
+        armv7*)
+            TARGET_ARCH=armhf
+            ;;
+        armel)
+            TARGET_ARCH=armel
+            ;;
+        x86_64|amd64)
+            TARGET_ARCH=amd64
+            ;;
+        i386|x86)
+            TARGET_ARCH=i386
+            ;;
+        *)
+            echo "Unsupported architecture. arch=$ARCH" >&2
+            exit 1
+            ;;
+    esac
+
+    cd /tmp
+    wget -q "https://github.com/thin-edge/tedge-standalone/releases/download/$VERSION/tedge-standalone-${TARGET_ARCH}.tar.gz"
+
+    mkdir -p "$INSTALL_PATH"
+    echo "Installing thin-edge.io to $INSTALL_PATH/tedge"
+    tar xzf /tmp/tedge-standalone-*.tar.gz -C "$INSTALL_PATH"
+    rm -f /tmp/tedge-standalone-*.tar.gz
+
+    echo
+    echo "Configure and start thin-edge.io using the following command:"
+    echo
+    echo "    $INSTALL_PATH/tedge/start.sh"
+    echo
+}
+
+main
